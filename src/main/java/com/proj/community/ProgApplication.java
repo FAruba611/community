a06963e660240e07517506ed0adffbc763a2fcbf
Windfall94 <franklee940611@gmail.com>
2019-09-22 22:54:48 +1000

    init repo

---
+++ src/main/java/com/proj/community/ProgApplication.java
@@ -0,0 +1,14 @@
+package com.proj.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProgApplication.class, args);
+	}
+
+}
